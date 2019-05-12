15639f1da5fd7037b7fccba858671c2fb9c16a61
Vishak Reviraj <vishakreviraj@gmail.com>
2019-05-12 05:37:10 +0530

    Updates

---
+++ precache-manifest.cdab8dc88e99d34ac75bf546869233cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6451d35aea1a7922c101450a0dcb71e8",
+    "url": "/greedygame-sample/index.html"
+  },
+  {
+    "revision": "1439651c271cb6c91b7f",
+    "url": "/greedygame-sample/static/css/main.b351f894.chunk.css"
+  },
+  {
+    "revision": "1d5ea22aa78f63acbc59",
+    "url": "/greedygame-sample/static/js/2.cb21a235.chunk.js"
+  },
+  {
+    "revision": "1439651c271cb6c91b7f",
+    "url": "/greedygame-sample/static/js/main.35483871.chunk.js"
+  },
+  {
+    "revision": "aa727078fd2636821457",
+    "url": "/greedygame-sample/static/js/runtime~main.971abd49.js"
+  }
+]);
